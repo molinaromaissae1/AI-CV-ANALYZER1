73c0b072ed08a503e2798d4b50e55d6275cadceb
molinaromaissae1 <molinaromaissae1@gmail.com>
2026-03-12 02:09:41 +0000

    Update reader.py

--- reader.py
+++ reader.py
@@ -1,26 +1,12 @@
 import pdfplumber
-import docx
 
-def read_cv(file):
+def extract_text_from_pdf(uploaded_file):
 
     text = ""
 
-    if file.name.endswith(".pdf"):
+    with pdfplumber.open(uploaded_file) as pdf:
 
-        with pdfplumber.open(file) as pdf:
-
-            for page in pdf.pages:
-
-                page_text = page.extract_text()
-
-                if page_text:
-                    text += page_text
-
-    elif file.name.endswith(".docx"):
-
-        doc = docx.Document(file)
-
-        for p in doc.paragraphs:
-            text += p.text
+        for page in pdf.pages:
+            text += page.extract_text()
 
     return text
