bc0df8cde66d45afff728b411b0c6b683ee93822
molinaromaissae1 <molinaromaissae1@gmail.com>
2026-03-13 21:03:05 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 
-from reader import extract_text_from_pdf
-from preprocess import preprocess_text
-
-from features import extract_education, extract_experience, extract_skills, extract_languages, extract_companies
+from features import (
+    extract_education,
+    extract_experience,
+    extract_skills,
+    extract_languages,
+    extract_companies
+)
 
 st.set_page_config(
     page_title="AI CV Analyzer",
