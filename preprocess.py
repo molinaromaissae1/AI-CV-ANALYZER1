282ef3f03f6cb808f452ddfa9fdbdcf2d7eb873c
Romaissae Molina <molinaromaissae1@gmail.com>
2026-03-19 01:15:31 +0000

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,12 +1,12 @@
-import re
-
 def preprocess_text(text):
+    import re
 
     text = text.lower()
 
+    # نحافظو على () و , مهمين بزاف
     text = re.sub(r'\n', ' ', text)
 
-    text = re.sub(r'[^a-zA-Z0-9àâçéèêëîïôûùüÿñæœ\s]', '', text)
+    text = re.sub(r'[^a-zA-Z0-9ââçéèêëîïôûùÿñæœ\s(),]', ' ', text)
 
     text = re.sub(r'\s+', ' ', text)
 
